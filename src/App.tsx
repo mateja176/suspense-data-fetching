0e86eb356dc06d6e908f5e84196ff7958d5efb07
Mateja Petrovic <ma.petrovic@levi9.com>
2019-10-28 18:12:21 +0100

    feat: install experimental package version and fetches users

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const getUsers = () =>
+  fetch('https://randomuser.me/api')
+    .then(res => res.json())
+    .then(({ results }) => results);
 
 const App: React.FC = () => {
+  const [users, setUsers] = React.useState([]);
+
+  React.useEffect(() => {
+    getUsers().then(setUsers);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {users.map(user => (
+          <li>
+            <pre>{JSON.stringify(user, null, 2)}</pre>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+(ReactDOM as any).createRoot(document.getElementById('root')).render(<App />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
